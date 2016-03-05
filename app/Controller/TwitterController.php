915a289c826f0012aee9c67933e18d3edc99d54f
h_shirai <h_shirai@nullpot.com>
2016-03-05 16:42:47 +0900

    Oauth認証の基本的な部分を作成

--- app/Controller/TwitterController.php
+++ app/Controller/TwitterController.php
@@ -23,10 +23,31 @@ class TwitterController extends AppController
 	public function getuser($userid = null)
 	{
 		$this->autoRender = false;
+		$this->autoLayout = false;
 
+		$twitter = new TwitterOAuth(
+			Configure::read('api.twitter.consumer_key'),
+			Configure::read('api.twitter.consumer_key_secret')
+		);
+		$request_token = $twitter->oauth(
+			'oauth/request_token',
+			array(
+				'oauth_callback' => 'http://tomodachi.nullpot.com/twitter/callback'
+			)
+		);
+		$url = $twitter->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+		$this->redirect($url);
 
 	}
 
+
+	public function callback()
+	{
+		$this->autoRender = false;
+		$this->autoLayout = false;
+		debug($this->request);
+
+	}
 
 }
 
